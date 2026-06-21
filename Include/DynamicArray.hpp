4b2408970305617b280779e150f51e7c872b3b5e
GlitchLabs <dakirthompson@gmail.com>
2026-06-20 20:29:15 -0400

    Created the push_back method for the dynamicArray class template
    Implemented dynamic growth with doubling stategy. Handles empty array edge case.

--- Include/DynamicArray.hpp
+++ Include/DynamicArray.hpp
@@ -10,6 +10,26 @@ public:
     DynamicArray(): m_size(0), m_capacity(0), m_data(nullptr){
 
     } 
+
+    /*
+    Params: Data to be stored.
+    Purpose: to add data to the back of the array.
+    Returns: None
+    */
+   void push_back(T data){
+    if(m_size == m_capacity){
+        //increae m_capacity by the doubling strategy
+        m_capacity = (m_capacity == 0) ? 1 :  m_capacity * 2;
+        T* newData = new T[m_capacity];
+        for(int i = 0; i < m_size; i++){
+            newData[i] = *(m_data+i);
+        }
+        delete[] m_data;
+        m_data = newData;
+    }
+    *(m_data + m_size) = data;
+    m_size++;
+    }
 private:
 size_t m_size;
 size_t m_capacity;
--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@
 
 int main(){
     std::cout<<"hello world";
+    DynamicArray<int> array;
+    array.push_back(70);
+    array.push_back(80);
+    array.push_back(30);
+    array.push_back(31459);
+    //thought this was interesting and wanted to talk about it. i think this is the powerfull part of C++ you can create anything and the hard part you have to create everything lol
+    //for( int elem : array)
+        //std::cout<<elem<<std::endl;
+    //for(int i = 0; i< array.size)
+    //wow cant do that either!!!
+    //do{int i = 0;}
+    //while(array[i] !=nullptr){
+
+    //}
     return 0;
 }
