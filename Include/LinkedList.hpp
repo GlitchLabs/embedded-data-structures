6db0246aab8482dc86151ab1db4f25577e600b67
GlitchLabs <dakirthompson@gmail.com>
2026-06-23 09:25:19 -0400

    Added a print function and destructor to the LInkedList templated class
    Teh linkedList class now frees the heap memory it uses before beign destroys. you can also print the current list.

---
+++ Include/LinkedList.hpp
@@ -0,0 +1,75 @@
+
+template<typename T>
+struct Node {
+    T value;
+    Node<T>* next = nullptr;
+};
+
+template<typename T>
+class LinkedList {
+public: 
+    /*
+    Constructor
+    */
+    LinkedList(): m_head(nullptr), m_tail(nullptr), m_size(0){
+    
+    }
+    size_t size()const{
+        return m_size;
+    }
+
+    /*
+    Params: data to be added to the LinkedList
+    Purpose: Add data to the back of the LinkedList and changing the tail
+    Returns: None
+    */
+    void push_back(T value) {
+        if(m_size == 0){
+            Node<T>* newNode = new Node<T>();
+            newNode->value = value;
+            m_head = newNode;
+            m_tail = newNode;
+            m_size++;
+            return;
+        }
+        Node<T>* newNode = new Node<T>();
+        newNode->value = value;
+        m_tail->next = newNode;
+        m_tail = newNode;
+        m_size++;
+        return;
+
+    }
+
+    /*
+    Params: None
+    Purpose: Prints the current contents of the LinkedList.
+    Returns: None
+    */
+    void print()const {
+        Node<T> *node = m_head;
+        while(node != nullptr){
+            std::cout<<node->value<<std::endl;
+            node = node->next;
+        }
+        return;
+    }
+
+    /*
+    Destructor
+    Purpose: to free all the heap memory used by this class
+    */
+    ~LinkedList(){
+        while(m_head != nullptr){
+            Node<T>* next = m_head->next;
+            delete m_head;
+            std::cout<<"Freed memory at "<<m_head<<"\n";
+            m_head = next;
+        }
+        return;
+    }
+private:
+    Node<T>* m_head;
+    Node<T>* m_tail;
+    size_t m_size;
+};
--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
 #include "DynamicArray.hpp"
+#include "LinkedList.hpp"
 
 int main(){
     std::cout<<"hello world"<<std::endl;
     DynamicArray<int> array;
-    array.push_back(70);
-    array.push_back(80);
-    array.push_back(30);
-    array.push_back(31459);
-    for(size_t i = 0; i < 4; i++)
-        std::cout << array.at(i) <<std::endl;
+    // array.push_back(70);
+    // array.push_back(80);
+    // array.push_back(30);
+    // array.push_back(31459);
+    // for(size_t i = 0; i < 4; i++)
+    //     std::cout << array.at(i) <<std::endl;
 
-    DynamicArray<int> array2 = array;
-    std::cout << "Copy: " << array2.at(0) <<std::endl;
+    // DynamicArray<int> array2 = array;
+    // std::cout << "Copy: " << array2.at(0) <<std::endl;
 
-    int* a = new int[3];//new pointer to int array memory
-    a[0] = 10; a[1] = 20; a[2] = 30;
+    // int* a = new int[3];//new pointer to int array memory
+    // a[0] = 10; a[1] = 20; a[2] = 30;
 
-    int* b = new int[3];//new pointer to memory holding contigous integer data
+    // int* b = new int[3];//new pointer to memory holding contigous integer data
 
-    for(int i = 0; i < 3; i++)
-        *(b + i) = *(a + i);
-    std::cout << b[0] << " , " << b[1] << " ," << b[2]<<std::endl;
-    delete[] a;
-    delete[] b;
+    // for(int i = 0; i < 3; i++)
+    //     *(b + i) = *(a + i);
+    // std::cout << b[0] << " , " << b[1] << " ," << b[2]<<std::endl;
+    // delete[] a;
+    // delete[] b;
 
-    for(size_t i = 0; i < array2.size(); i++)
-        std::cout<<array2[i] <<std::endl;
+    // for(size_t i = 0; i < array2.size(); i++)
+    //     std::cout<<array2[i] <<std::endl;
+
+    LinkedList<char> encryptedMessages;
+    encryptedMessages.push_back('r');
+    encryptedMessages.push_back('8');
+    encryptedMessages.push_back('L');
+    encryptedMessages.push_back('!');
+    std::cout <<encryptedMessages.size()<<std::endl;
+    encryptedMessages.print();
     return 0;
 }
